1dc3fd72ecd6b0bd07a2f1afa308b868c8279b07
Maciej Zapart <maciej.zapart.96@gmail.com>
2019-06-01 18:58:05 +0200

    added sign up screen, custom splash screen

---
+++ screens/SignUpScreen.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { View, ScrollView, StyleSheet, Text, Image } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
+
+
+export default class LinksScreen extends React.Component {
+  static navigationOptions = {
+    title: "Sign In With Email"
+  };
+  constructor(props) {
+    super(props);
+    this.state = { email: '' };
+    this.state = { password: ''}
+  }
+  render() {
+    return (
+      <>
+        
+
+      <ScrollView style={styles.container}>
+        <Text>Email:</Text>
+       <TextInput
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(email) => this.setState({email})}
+        value={this.state.email}
+      />
+      <Text>Password:</Text>
+      <TextInput secureTextEntry={true}
+        style={{height: 40, borderColor: 'gray', borderWidth: 1}}
+        onChangeText={(password) => this.setState({password})}
+        value={this.state.password}
+      />
+      </ScrollView>
+      <View style={{alignContent: 'center', alignItems: 'center'}}>
+      <Text style={{paddingBottom:'7%'}}>Or Sign in With Social Media</Text>
+      <Image style={{padding:'2%'}}  source={require('../assets/images/facebook.png')} />
+      <Image style={{paddingBottom:'2%'}} source={require('../assets/images/google.png')}/>
+    
+      </View>
+      </>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ 
+  container: {
+    flex: 1,
+    paddingTop: 5,
+    backgroundColor: "#fff",
+    margin: 0
+  }
+});
